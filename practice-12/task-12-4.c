e3160fb7b4aaa01978c9f2943ea9e3144c065888
karinasergeeva77 <karinaa.sergeeva.2002@mail.ru>
2026-04-03 22:06:46 +0300

    Create task-12-4.c

---
+++ practice-12/task-12-4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int counter = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *increment(void *arg) {
+    for (int i = 0; i < 100000; i++) {
+        pthread_mutex_lock(&mutex);
+        counter++;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+int main(void) {
+    pthread_t t1, t2;
+    
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+    
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    
+    printf("Ожидаемое значение: 200000\n");
+    printf("Фактическое значение: %d\n", counter);
+    
+    pthread_mutex_destroy(&mutex);
+    return 0;
+}
